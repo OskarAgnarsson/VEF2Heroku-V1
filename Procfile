6e50a94a5daabae7666610ba9683db0cc467048e
OskarAgnarsson <oskarag309@nemi.tskoli.is>
2018-09-03 16:01:32 +0000

    Heroku setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python Verkefni1.py
---
+++ Verkefni1.py
@@ -0,0 +1,27 @@
+from sys import argv
+
+from bottle import *
+
+@route("/")
+
+def index():
+    return """
+    <h1>Verkefni 1</h1>
+    <a href='/stuff'>stuff</a>
+    <a href='/stuff1'>stuff1</a>
+    <a href='/stuff2'>stuff2</a>
+    """
+
+@route("/stuff")
+def stuff():
+    return "<h2>Hérna er stuff</h2>"
+
+@route("/stuff1")
+def stuff():
+    return "<h2>Hérna er meira stuff</h2>"
+
+@route("/stuff2")
+def stuff():
+    return "<h2>Hérna er enn meira stuff</h2>"
+
+run(host="localhost", port=8080, debug=True)
